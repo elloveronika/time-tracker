6765dc3f1f81b01b5937c9b423cc09290815833c
elloveronika <ello.veronika92@gmail.com>
2022-02-18 22:54:42 -0800

    added pfp

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <img src="/images/eugene.jpg" alt="eugenevsarah" />
+      <span>Report for:</span>
+      <h2> Eugene V Sarah</h2>
+    </div>
+  );
+};
+export default About;
---
+++ src/components/Activity.js
@@ -0,0 +1,4 @@
+const Activity = () => {
+  return <h2> this is Activity, hi</h2>;
+};
+export default Activity;
