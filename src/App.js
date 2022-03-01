993705a8c9380bd8738f83c698eb9cd176c85e53
elloveronika <ello.veronika92@gmail.com>
2022-03-01 01:02:55 -0800

    working on fixing state bugs

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import About from "./components/About";
 import Activity from "./components/Activity";
 import "./styles.css";
 import data from "./data.json";
 
-console.log(
-  "this is data",
-  data.map((el) => el.title)
-);
+// console.log(
+//   "this is data",
+//   data.map((el) => el.title)
+// );
 const App = () => {
+  const [timeFrame, setTimeFrame] = useState(data);
+
+  // console.log("this is timeframe", timeFrame);
+  let mappedTimeFrame = timeFrame.map((time) => time.timeframes);
+  console.log("this is mappedTimeFrame", mappedTimeFrame);
+
+  const handleClickDaily = () => {
+    console.log("hi this is click daily :3");
+
+    let mappedTimeFrame = timeFrame.map((time) => time.timeframes);
+    setTimeFrame((prevTime) => prevTime.map((time) => time.timeframes.daily));
+
+  };
+
   return (
     <div className="app">
-      <About />
+      <About someFunc={handleClickDaily} />
       <div className="app--activity">
-      {data.map((el) => (
-        <Activity data={el} />
-      ))}
+        {data.map((el) => (
+          <Activity title={el.title} timeFrame={el.timeframes} />
+        ))}
       </div>
     </div>
   );
--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import images from "../images/image-jeremy.png";
 
-const About = () => {
+const About = (props) => {
   // console.log(images);
   return (
-    <div className="about">
-      <img src={images} alt="eugenevsarah" />
-      <span>Report for:</span>
-      <h2> Eugene V Sarah</h2>
+    <div className="about--main">
+      <div className="about">
+        <img src={images} alt="eugenevsarah" />
+        <span>Report for:</span>
+        <h2> Eugene V Sarah</h2>
+      </div>
+      <div className="about--tracker"></div>
+      <h3 onClick={() => props.someFunc}>Daily</h3>
+      <h3>Weekly</h3>
+      <h3>Monthly</h3>
     </div>
   );
 };
