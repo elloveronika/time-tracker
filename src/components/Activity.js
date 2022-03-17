88c5706016246ed3c44dca6d69c0c11535ad2b17
elloveronika <ello.veronika92@gmail.com>
2022-03-16 23:47:12 -0700

    added icon rendering not dont yet

--- src/components/Activity.js
+++ src/components/Activity.js
@@ -1,19 +1,37 @@
+import play from "../images/icon-play.svg";
+import work from "../images/icon-work.svg";
+import selfcare from "../images/icon-self-care.svg";
+
 const Activity = (props) => {
   console.log("this is props inside activity", props.time);
   let time = props.time;
+
+  const styles = {
+    backgroundImage: `url(${
+      props.data.title.toLowerCase() == "play" ? play : work
+    })`,
+    // zIndex: 3,
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "right top",
+    // justifyContent: "flex-end",
+  };
+  console.log("this is props title", props.data.title);
+
   return (
-    <div className="activity">
-      <h2>{props.data.title} </h2>
-      <div className="activity--weekly">
-        <h2>{props.data.timeframes[time].current}hrs</h2>
-        <h3>
-          {props.time == "weekly"
-            ? "Last Week"
-            : props.time == "daily"
-            ? "Yesterday"
-            : "Last Month"}
-          : {props.data.timeframes[time].previous}hrs
-        </h3>
+    <div className="icon-activity" style={styles}>
+      <div className="activity">
+        <h2>{props.data.title} </h2>
+        <div className="activity--weekly">
+          <h2>{props.data.timeframes[time].current}hrs</h2>
+          <h3>
+            {props.time == "weekly"
+              ? "Last Week"
+              : props.time == "daily"
+              ? "Yesterday"
+              : "Last Month"}
+            : {props.data.timeframes[time].previous}hrs
+          </h3>
+        </div>
       </div>
     </div>
   );
